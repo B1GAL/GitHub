724db2bb6917a1e3f0de44e846d672d6e5b65469
Alex Upton <alexander99w@ou.edu>
2021-08-24 18:21:03 -0500

    Done with first Participation

--- FirstParticipation/Program.cs
+++ FirstParticipation/Program.cs
@@ -7,7 +7,7 @@ namespace FirstParticipation
         static void Main(string[] args)
         {
             Console.WriteLine("My comfort level with programming is about a 4/10 its pretty shaky and taking a break from it during the summer did not help." );
-            Console.WriteLine(");
+            Console.WriteLine("I learned all the basics of C# from MIS 3013 although im not very good at putting them into practice");
         }
     }
 }
