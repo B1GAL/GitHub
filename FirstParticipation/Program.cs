3260e4498a7cfb24b96db239ea8c1147d8e39091
Alex Upton <alexander99w@ou.edu>
2021-08-24 18:19:57 -0500

    Add project files.

---
+++ FirstParticipation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+//Alex Upton
+namespace FirstParticipation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My comfort level with programming is about a 4/10 its pretty shaky and taking a break from it during the summer did not help." );
+            Console.WriteLine(");
+        }
+    }
+}
